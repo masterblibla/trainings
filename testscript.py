ff7a4f7f1628d0ac206ec9f313cf94f56cec529b
André Meyer <andrealexander.meyer@outloook.com>
2021-04-22 21:08:31 +0200

    Übung 2

---
+++ testscript.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 21 18:38:48 2021
+
+@author: andre
+"""
+
+
+
+def funktioon(a,b=10):
+    '''
+    
+
+    Parameters
+    ----------
+    a : int
+        Number of chickens i own. 
+    b : TYPE, optional
+        DESCRIPTION. The default is 10.
+
+    Returns
+    -------
+    b : TYPE
+        DESCRIPTION.
+
+    '''
+        
+
+    
+    
+    c = a*b
+    
+    return c
+
+funktioon(19,4)
